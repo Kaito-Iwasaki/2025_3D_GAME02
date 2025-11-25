38e296640f56180e5b4ae66e6663a630e7da5c82
Kaito-Iwasaki <251307@st.yoshida-g.ac.jp>
2025-11-26 05:06:20 +0900

    スフィア行けそうかも。あとバッファサイズとインデックスの設定
    なんか多分バグってたのは頂点座標の設定順とか頂点ポインタのずらし方に問題があったんだと思ふ。breakで抜けると更新処理が処理されねぇ

--- sphere.cpp
+++ sphere.cpp
@@ -19,7 +19,7 @@
 // 
 //*********************************************************************
 #define TEXTURE_FILENAME	"data\\TEXTURE\\sakai000.png"
-#define INIT_POS			D3DXVECTOR3(0.0f, 50.0f, 0.0f)
+#define INIT_POS			D3DXVECTOR3(0.0f, 150.0f, 0.0f)
 #define INIT_SIZE			D3DXVECTOR3(100.0f, 100.0f, 100.0f)
 #define INIT_COLOR			D3DXCOLOR_WHITE
 #define NUM_BLOCK_X			(3)
@@ -66,7 +66,7 @@ void InitSphere(void)
 
 	ZeroMemory(pSphere, sizeof(pSphere));
 
-	SetSphere(0, D3DXVECTOR3_ZERO, INIT_SIZE, NUM_BLOCK_X, NUM_BLOCK_Y);
+	SetSphere(0, INIT_POS, INIT_SIZE, NUM_BLOCK_X, NUM_BLOCK_Y);
 }
 
 //=====================================================================
@@ -237,94 +237,44 @@ void SetSphere(int nTexId, D3DXVECTOR3 pos, D3DXVECTOR3 size, int nSegmentU, int
 		);
 
 		VERTEX_3D* pVtx;
-		D3DXVECTOR3 vecOrigin = D3DXVECTOR3(0.0f, pSphere->obj.size.y / 2, 0.0f);
 
 		// 頂点バッファをロックして頂点情報へのポインタを取得
 		pSphere->pVtxBuff->Lock(0, 0, (void**)&pVtx, 0);
 
-		int nCount = 0;
-
-		pVtx[0].pos = D3DXVECTOR3(0.0f, 90.0f, 0.0f);
-
-		pVtx[1].pos = D3DXVECTOR3(sinf(D3DXToRadian(0)) * 50.0f, 60.0f, cosf(D3DXToRadian(0)) * 50.0f);
-		pVtx[2].pos = D3DXVECTOR3(sinf(D3DXToRadian(-120)) * 50.0f, 60.0f, cosf(D3DXToRadian(-120)) * 50.0f);
-		pVtx[3].pos = D3DXVECTOR3(sinf(D3DXToRadian(-240)) * 50.0f, 60.0f, cosf(D3DXToRadian(-240)) * 50.0f);
-		pVtx[4].pos = D3DXVECTOR3(sinf(D3DXToRadian(0)) * 50.0f, 60.0f, cosf(D3DXToRadian(0)) * 50.0f);
-
-		pVtx[5].pos = D3DXVECTOR3(sinf(D3DXToRadian(0)) * 50.0f, 30.0f, cosf(D3DXToRadian(0)) * 50.0f);
-		pVtx[6].pos = D3DXVECTOR3(sinf(D3DXToRadian(-120)) * 50.0f, 30.0f, cosf(D3DXToRadian(-120)) * 50.0f);
-		pVtx[7].pos = D3DXVECTOR3(sinf(D3DXToRadian(-240)) * 50.0f, 30.0f, cosf(D3DXToRadian(-240)) * 50.0f);
-		pVtx[8].pos = D3DXVECTOR3(sinf(D3DXToRadian(0)) * 50.0f, 30.0f, cosf(D3DXToRadian(0)) * 50.0f);
-
-		pVtx[9].pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-
-		//pVtx[0].nor = Normalize(pVtx[0].pos - pSphere->obj.pos);
-		//pVtx[1].nor = Normalize(pVtx[1].pos - pSphere->obj.pos);
-		//pVtx[2].nor = Normalize(pVtx[2].pos - pSphere->obj.pos);
-		//pVtx[3].nor = Normalize(pVtx[3].pos - pSphere->obj.pos);
-		//pVtx[4].nor = Normalize(pVtx[4].pos - pSphere->obj.pos);
-		//pVtx[5].nor = Normalize(pVtx[5].pos - pSphere->obj.pos);
-		//pVtx[6].nor = Normalize(pVtx[6].pos - pSphere->obj.pos);
-		//pVtx[7].nor = Normalize(pVtx[7].pos - pSphere->obj.pos);
-		//pVtx[8].nor = Normalize(pVtx[8].pos - pSphere->obj.pos);
-		//pVtx[9].nor = Normalize(pVtx[9].pos - pSphere->obj.pos);
-
-		//pVtx[0].col = pSphere->obj.color;
-		//pVtx[1].col = pSphere->obj.color;
-		//pVtx[2].col = pSphere->obj.color;
-		//pVtx[3].col = pSphere->obj.color;
-		//pVtx[4].col = pSphere->obj.color;
-		//pVtx[5].col = pSphere->obj.color;
-		//pVtx[6].col = pSphere->obj.color;
-		//pVtx[7].col = pSphere->obj.color;
-		//pVtx[8].col = pSphere->obj.color;
-		//pVtx[9].col = pSphere->obj.color;
-
-		pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
-		pVtx[1].tex = D3DXVECTOR2(0.0f, 0.3f);
-		pVtx[2].tex = D3DXVECTOR2(0.3f, 0.3f);
-		pVtx[3].tex = D3DXVECTOR2(0.6f, 0.3f);
-		pVtx[4].tex = D3DXVECTOR2(1.0f, 0.3f);
-
-		pVtx[5].tex = D3DXVECTOR2(0.0f, 0.6f);
-		pVtx[6].tex = D3DXVECTOR2(0.3f, 0.6f);
-		pVtx[7].tex = D3DXVECTOR2(0.6f, 0.6f);
-		pVtx[8].tex = D3DXVECTOR2(1.0f, 0.6f);
-
-		pVtx[9].tex = D3DXVECTOR2(1.0f, 1.0f);
-
-		// 頂点情報を設定
 		for (int nCntVtxV = 0; nCntVtxV < nSegmentV + 1; nCntVtxV++)
 		{
-			for (int nCntVtxU = 0; nCntVtxU < nSegmentU + 1; nCntVtxU++, pVtx++)
+			float fAngleV = D3DX_PI / nSegmentV * nCntVtxV;
+
+			for (int nCntVtxU = 0; nCntVtxU < nSegmentU + 1; nCntVtxU++)
 			{
+				float fAngleU = (D3DX_PI * 2.0f) / nSegmentU * nCntVtxU;
+
 				if (nCntVtxV == 0)
 				{
-					pVtx->pos = vecOrigin;
+					pVtx->pos = D3DXVECTOR3(0.0f, pSphere->obj.size.y / 2, 0.0f);;
 					pVtx->tex = D3DXVECTOR2(0.0f, 0.0f);
 				}
 				else if (nCntVtxV == nSegmentV)
 				{
-					pVtx->pos = vecOrigin - D3DXVECTOR3(0.0f, pSphere->obj.size.y, 0.0f);
+					pVtx->pos = D3DXVECTOR3(0.0f, -pSphere->obj.size.y / 2, 0.0f);
 					pVtx->tex = D3DXVECTOR2(1.0f, 1.0f);
-
 				}
 				else
 				{
-					float fAngle = (D3DX_PI * 2) / nSegmentU * nCntVtxU;
-					pVtx->pos.x = -sinf(fAngle) * pSphere->obj.size.x / 2.0f;
-					pVtx->pos.z = cosf(fAngle) * pSphere->obj.size.z / 2.0f;
+					pVtx->pos = D3DXVECTOR3(sinf(fAngleU) * 50.0f, cosf(fAngleV) * 50.0f, cosf(fAngleU) * 50.0f);
 					pVtx->tex = D3DXVECTOR2((float)nCntVtxU / (float)nSegmentU, (float)nCntVtxV / (float)nSegmentV);
 				}
 
-				pVtx->nor = Normalize(pVtx->pos - pSphere->obj.pos);
+				pVtx->nor = Normalize(pVtx->pos);
 				pVtx->col = pSphere->obj.color;
-				nCount++;
+
+				pVtx++;
 
 				if (nCntVtxV == 0 || nCntVtxV == nSegmentV)
 				{
 					break;
 				}
+
 			}
 		}
 
@@ -333,11 +283,15 @@ void SetSphere(int nTexId, D3DXVECTOR3 pos, D3DXVECTOR3 size, int nSegmentU, int
 
 		WORD* pIdx;		// インデックス情報へのポインタ
 
+		int nSize = (1 + nSegmentU + 1) * 2 + 2 * (nSegmentU + 1) * (nSegmentV - 2);
+
+		printf("a");
+
 		// インデックスバッファの生成
 		// （インデックスバッファのサイズは、メッシュでのポリゴン描画に必要な分用意する）
-		// （上面と底面の頂点数＋２頂点×横の頂点数×縦の分割数＋２頂点×横に折り返す回数）
+		// 
 		pDevice->CreateIndexBuffer(
-			sizeof(WORD) * 18,
+			sizeof(WORD) * nSize,
 			D3DUSAGE_WRITEONLY,
 			D3DFMT_INDEX16,
 			D3DPOOL_MANAGED,
@@ -349,55 +303,30 @@ void SetSphere(int nTexId, D3DXVECTOR3 pos, D3DXVECTOR3 size, int nSegmentU, int
 		pSphere->pIdxBuff->Lock(0, 0, (void**)&pIdx, 0);
 
 		pIdx[0] = 0;
-		pIdx[1] = 4;
-		pIdx[2] = 3;
-		pIdx[3] = 2;
-		pIdx[4] = 1;
-		pIdx[5] = 5;
-		pIdx[6] = 1;
-		pIdx[7] = 6;
-		pIdx[8] = 2;
-		pIdx[9] = 7;
-		pIdx[10] = 3;
-		pIdx[11] = 8;
-		pIdx[12] = 4;
+		pIdx[1] = 1;
+		pIdx[2] = 2;
+		pIdx[3] = 3;
+		pIdx[4] = 4;
+		pIdx[5] = 8;
+		pIdx[6] = 4;
+		pIdx[7] = 7;
+		pIdx[8] = 3;
+		pIdx[9] = 6;
+		pIdx[10] = 2;
+		pIdx[11] = 5;
+		pIdx[12] = 1;
 		pIdx[13] = 9;
-		pIdx[14] = 5;
-		pIdx[15] = 6;
-		pIdx[16] = 7;
-		pIdx[17] = 8;
+		pIdx[14] = 8;
+		pIdx[15] = 7;
+		pIdx[16] = 6;
+		pIdx[17] = 5;
 
-		// 頂点番号データの設定
-		//for (int nCntIdxV = 0; nCntIdxV < nSegmentV - 1; nCntIdxV++)
+		//for (int nCntIdxV = 0; nCntIdxV < nSegmentV; nCntIdxV++)
 		//{
+		//	for (int nCntIdxU = 0; nCntIdxU < nSegmentU + 1; nSegmentU++)
+		//	{
 
-
-
-			//if (nCntIdxV == 0)
-			//{
-			//	*pIdx = 0;
-			//	pIdx++;
-
-			//	for (int nCntIdxU = 0; nCntIdxU < nSegmentU; nCntIdxU++)
-			//	{
-			//		*pIdx = nCntIdxU + 1;
-			//		pIdx++;
-			//	}
-			//}
-			//else if (nCntIdxV == nSegmentV - 2)
-			//{
-			//	for (int nCntIdxU = 0; nCntIdxU < nSegmentU; nCntIdxU++)
-			//	{
-			//		*pIdx = 1 + nCntIdxU + (pSphere->nSegmentX * (nCntIdxV - 1));
-			//		pIdx++;
-			//	}
-
-			//	*pIdx = 1 + pSphere->nSegmentX * nCntIdxV;
-			//}
-			//else
-			//{
-
-			//}
+		//	}
 		//}
 
 		// インデックスバッファをアンロック
